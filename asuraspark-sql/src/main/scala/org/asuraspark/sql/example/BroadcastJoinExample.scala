5510cdc4306aa938a28fae44a0fb9227e6452b85
asura7969 <1402357969@qq.com>
2020-10-10 00:33:40 +0800

    spark broadcast test

--- asuraspark-sql/src/main/scala/org/asuraspark/sql/example/BroadcastJoinExample.scala
+++ asuraspark-sql/src/main/scala/org/asuraspark/sql/example/BroadcastJoinExample.scala
@@ -1,8 +1,11 @@
 package org.asuraspark.sql.example
 
+import org.apache.spark.scheduler.{SparkListener, SparkListenerEvent}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.joins.{BroadcastHashJoinExec, BroadcastNestedLoopJoinExec, SortMergeJoinExec}
+//import org.apache.spark.sql.execution.ui.SparkListenerSQLAdaptiveExecutionUpdate
 import org.apache.spark.sql.functions.{broadcast, soundex}
+import org.apache.spark.sql.internal.SQLConf.{ADAPTIVE_EXECUTION_ENABLED, AUTO_BROADCASTJOIN_THRESHOLD}
 
 import scala.util.{Failure, Try}
 
@@ -17,10 +20,30 @@ object BroadcastJoinExample {
     val spark = SparkSession.builder()
       .master("local[*]")
       .appName("broadcast join example")
+      .config(ADAPTIVE_EXECUTION_ENABLED.key,"true")
+//      .config(AUTO_BROADCASTJOIN_THRESHOLD.key,"100MB")
       .getOrCreate()
+
+    var finalPlanCnt = 0
+//    val listener = new SparkListener {
+//      override def onOtherEvent(event: SparkListenerEvent): Unit = {
+//        event match {
+//          case SparkListenerSQLAdaptiveExecutionUpdate(_, _, sparkPlanInfo) =>
+//            if (sparkPlanInfo.simpleString.startsWith(
+//              "AdaptiveSparkPlan isFinalPlan=true")) {
+//              finalPlanCnt += 1
+//            }
+//          case _ => // ignore other events
+//        }
+//      }
+//    }
+//    spark.sparkContext.addSparkListener(listener)
+
+
 
     // 模拟 bradcast join OOM
     val larger = spark.range(20000000).as('id)
+//    val larger = spark.range(200).as('id)
     val small = spark.range(100000000).as('id)
 
 //    val broadcastJoinDF = larger.join(broadcast(small), Seq("id"))
@@ -39,12 +62,21 @@ object BroadcastJoinExample {
         |
         |""".stripMargin)
 
+//    val joinResultDF = spark.sql(
+//      """
+//        |
+//        |select *
+//        |FROM larger JOIN small
+//        |ON larger.id = small.id
+//        |
+//        |""".stripMargin)
 
-    val physical = joinResultDF.queryExecution.sparkPlan
 
-    println(physical.toString())
+    val plan = joinResultDF.queryExecution.executedPlan
 
-    val operators = physical.collect{
+    println(plan.toString())
+
+    val operators = plan.collect{
       case j: BroadcastHashJoinExec => j
       case j: BroadcastNestedLoopJoinExec => j
       case a => a
